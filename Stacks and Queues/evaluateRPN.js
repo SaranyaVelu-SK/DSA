fb00f47a2f4788eb7579b564df33f1c60a339744
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-08-04 23:17:53 +0530

    next greater element

---
+++ Stacks and Queues/evaluateRPN.js
@@ -0,0 +1,20 @@
+var evalRPN = function(tokens) {
+    let stack=[];
+    let map={
+        '+':(a,b)=>(b+a),
+        '-':(a,b)=>(b-a),
+        '*':(a,b)=>(b*a),
+        '/':(a,b)=> Math.trunc(b/a),
+    }
+    for(let i=0;i<tokens.length;i++){
+        if(map[tokens[i]]){
+            let a = stack.pop();
+            let b = stack.pop();
+            let ans = map[tokens[i]](Number(a),Number(b));
+            stack.push(ans);
+        }else{
+            stack.push(tokens[i])
+        }
+    }
+    return Number(stack.pop());
+};
