01b1aef41ebf98777e62bb6863cea5bdedd03ae8
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-10-06 21:37:59 +0530

    added brute and better code for few problems

--- Arrays/missingNumber.js
+++ Arrays/missingNumber.js
@@ -1,5 +1,37 @@
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
+
+//brute force
+var missingNumber = function(nums) {
+    let n = nums.length;
+   for(let i=0;i<=n;i++){
+    let flag =0;
+    for(let x=0;x<n;x++){
+        if(nums[x]==i){
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0){
+        return i
+    }
+   }
+};
+//better
+var missingNumber = function(nums) {
+    let n = nums.length;
+   let arr = new Array(n+1).fill(0);
+
+   for(let i=0;i<n;i++){
+    arr[nums[i]]=1
+   }
+   for(let x=0;x<=n;x++){
+    if(arr[x]==0){
+        return x;
+    }
+   }
+};
+//optimal
 function findMissingNumber(arr){
     let n = arr.length;
     let totalSum = (n*(n+1))/2;
@@ -9,3 +41,16 @@ function findMissingNumber(arr){
     }
     return totalSum-sum
 }
+
+var missingNumber = function(nums) {
+    let n=nums.length
+let x1=0;
+let x2=0;
+
+for(let i=0;i<n;i++){
+    x1 = x1^ nums[i];
+    x2 = x2 ^ (i+1);
+}
+
+return x1^x2
+};
--- Arrays/singleNumber.js
+++ Arrays/singleNumber.js
@@ -2,6 +2,39 @@
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
+
+// brute force
+var singleNumber = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        let num = nums[i];
+        let count =0;
+        for(let x=0;x<nums.length;x++){
+            if(nums[x]==num){
+                count++
+            }
+        }
+        if(count ==1){
+            return num
+        }
+    }
+};
+//better
+var singleNumber = function(nums) {
+    let hashMap = {};
+    for(let i=0;i<nums.length;i++){
+        if(!hashMap[nums[i]]){
+            hashMap[nums[i]] =1;
+        }else{
+            hashMap[nums[i]]++;
+        }
+    }
+    for(let key in hashMap){
+        if(hashMap[key]==1){
+            return Number(key)
+        }
+    }
+};
+//optimal
 function singleNumber(nums) {
     let xor=0;
     for(let i=0;i<nums.length;i++){
