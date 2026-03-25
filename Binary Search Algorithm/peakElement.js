beca9ea632fe53a302c13ac2d82f9121b196dd76
SaranyaSK <saranyakulanthaivelu@gmail.com>
2026-03-25 12:57:51 +0530

    linear search to find peak element and optimized binary search

--- Binary Search Algorithm/peakElement.js
+++ Binary Search Algorithm/peakElement.js
@@ -1,14 +1,28 @@
 var findPeakElement = function(nums) {
-    let l=0;
-    let r = nums.length-1;
+    let n=nums.length;
+    if(n==1 || nums[0]>nums[1]) return 0;
+    if(nums[n-1]>nums[n-2]) return n-1;
+    let l=1;
+    let r=n-2;
     while(l<r){
-        let m=l+Math.floor((r-l)/2);
-
+        let m = l+Math.floor((r-l)/2);
         if(nums[m]<nums[m+1]){
             l=m+1;
         }else{
-            r=m
+            r=m;
         }
     }
     return l;
+};
+
+// linear search
+var findPeakElement = function(nums) {
+    if(nums.length==1 || nums[0]>nums[1]) return 0;
+    if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+    for(let i=1;i<nums.length-1;i++){
+        if(nums[i]>nums[i-1]  && nums[i]>nums[i+1]){
+            return i;
+        }
+    }
+
 };
